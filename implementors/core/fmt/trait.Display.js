1399d34dfa436ac7abdc0f44ff53096fd6f80a84
Travis CI User <travis@example.org>
2018-11-28 22:54:44 +0000

    Documentation for hrektts/cdr-rs@5644eb0

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["cdr"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"cdr/struct.Error.html\" title=\"struct cdr::Error\">Error</a>",synthetic:false,types:["cdr::error::Error"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"cdr/enum.ErrorKind.html\" title=\"enum cdr::ErrorKind\">ErrorKind</a>",synthetic:false,types:["cdr::error::ErrorKind"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/failure/trait.Fail.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["cdr"] = [{text:"impl Fail for <a class=\"enum\" href=\"cdr/enum.ErrorKind.html\" title=\"enum cdr::ErrorKind\">ErrorKind</a>",synthetic:false,types:["cdr::error::ErrorKind"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
